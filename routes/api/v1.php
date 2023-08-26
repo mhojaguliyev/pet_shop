729bd0f1c3e98dd9ad916f541d21aa3be9f286d0
Merdan Hojaguliyev <mhojaguliyev@gmail.com>
2023-08-26 21:53:43 +0500

    standardize routes

--- routes/api/v1.php
+++ routes/api/v1.php
@@ -2,25 +2,27 @@
 
 use App\Http\Controllers\Api\v1\CategoryController;
 use App\Http\Controllers\Api\v1\ProductController;
-use App\Http\Controllers\api\v1\User\AuthController;
+use App\Http\Controllers\api\v1\User\AuthController as UserAuthController;
 use Illuminate\Support\Facades\Route;
 
-// user routes
-Route::prefix('user')->group(function () {
+Route::group(['as' => 'api.'], function () {
     // auth routes
-    Route::post('login', [AuthController::class, 'login']);
-    Route::post('logout', [AuthController::class, 'logout']);
-    Route::get('/', [AuthController::class, 'profile']);
+    Route::post('user/login', [UserAuthController::class, 'login']);
+    Route::post('user.logout', [UserAuthController::class, 'logout']);
+
+
+    // user routes
+    Route::get('user', [UserAuthController::class, 'profile']);
+
+
+    // category routes
+    Route::get('categories', [CategoryController::class, 'index']);
+
+
+    // product routes
+    Route::get('products', [ProductController::class, 'index']);
+    Route::post('product/create', [ProductController::class, 'create']);
+    Route::get('product/{product:uuid}', [ProductController::class, 'show']);
+    Route::put('product/{product:uuid}', [ProductController::class, 'update']);
+    Route::delete('product/{product:uuid}', [ProductController::class, 'destroy']);
 });
-
-
-// category routes
-Route::get('categories', [CategoryController::class, 'index']);
-
-
-// product routes
-Route::get('products', [ProductController::class, 'index']);
-Route::post('product/create', [ProductController::class, 'create']);
-Route::get('product/{product:uuid}', [ProductController::class, 'show']);
-Route::put('product/{product:uuid}', [ProductController::class, 'update']);
-Route::delete('product/{product:uuid}', [ProductController::class, 'destroy']);
